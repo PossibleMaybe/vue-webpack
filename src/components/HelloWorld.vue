522a6adcc7c6ddc413101813600ba7d3ebce0d57
PossibleMaybe <869953940@qq.com>
2017-12-11 20:05:11 +0800

    add less

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+   <div class="outer">
+     <div class="inner">
+       
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .outer {
+    width: 200px;
+    height: 200px;
+    background: red;
+    .inner {
+      width: 100px;
+      height: 100px;
+      background: yellow;
+    }
+  }
+</style>
